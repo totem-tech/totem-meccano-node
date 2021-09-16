5140308ff63da42d10287b3483d6f508cea81388
Chris D'Costa <chris.dcosta@totemaccounting.com>
2021-09-16 22:47:41 +0200

    small bug fixes

--- core/test-runtime/wasm/build.sh
+++ core/test-runtime/wasm/build.sh
@@ -7,9 +7,8 @@ else
 	CARGO_CMD="cargo +nightly"
 fi
 CARGO_INCREMENTAL=0 RUSTFLAGS="-C link-arg=--export-table" $CARGO_CMD build --target=wasm32-unknown-unknown --release
-for i in substrate_test_runtime
-do
-	# End of life for wasm-gc
-	# wasm-gc target/wasm32-unknown-unknown/release/$i.wasm target/wasm32-unknown-unknown/release/$i.compact.wasm
-	mv target/wasm32-unknown-unknown/release/$i.wasm target/wasm32-unknown-unknown/release/$i.compact.wasm
-done
+# End of life for wasm-gc
+# for i in substrate_test_runtime
+# do
+# 	wasm-gc target/wasm32-unknown-unknown/release/$i.wasm target/wasm32-unknown-unknown/release/$i.compact.wasm
+# done
